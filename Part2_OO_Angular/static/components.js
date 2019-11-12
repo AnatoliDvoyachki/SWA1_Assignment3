1b3abecd490e3fe6b2d0b58d850c2869b7d4a3f6
Anatoli <anatolidvoyachki@gmail.com>
2019-11-12 12:34:41 +0100

    Reloading of data works now

--- Part2_OO_Angular/static/components.js
+++ Part2_OO_Angular/static/components.js
@@ -16,38 +16,45 @@ module.value('$model', { salary: 0, cityNames: ["Aarhus", "Horsens", "Copenhagen
 module.controller('WeatherDataController', function($scope, $model, $http) {
       $scope.model = $model
       let aModel
-      $http.get('http://localhost:8080/data/')
-           .then(({data: weatherData}) => {
-            $http.get('http://localhost:8080/forecast/')
-                 .then(({data: weatherPredictionData}) => {
-                      aModel = model(weatherData, weatherPredictionData)
-                      $scope.model.latestDataOfEachType = aModel.showLatestWeatherData()
-                      $scope.model.minimumTemperatureData = aModel.showMinimumTemperatureWeatherData()
-                      $scope.model.maximumTemperatureData = aModel.showMaximumTemperatureWeatherData()
-                      $scope.model.totalPrecipitation = aModel.showTotalPrecipitation()
-                      $scope.model.averageWindSpeed = aModel.showAverageWindSpeed()
-                      $scope.model.averageCloudCoverage = aModel.showAverageCloudCoverage()
-                      $scope.model.dominantWindDirection = aModel.showDominantWindDirection() 
-                      $scope.model.weatherPredictions = aModel.showWeatherForecastData()
-              })         
-      }) 
+      loadData(aModel, $scope, $http)
 
       $scope.onCitySelectionChanged = () => {
-        const cityName = $scope.cityName
-        $http.get('http://localhost:8080/data/' + cityName)
+        loadData(aModel, $scope, $http)
+      }
+
+      $scope.onReloadDataClicked = () => {
+          loadData(aModel, $scope, $http)
+      }
+})
+
+
+function loadData(aModel, $scope, $http) {
+      let weatherDataUrl = 'http://localhost:8080/data/'
+      let weatherForecastUrl = 'http://localhost:8080/forecast/'
+  
+      const cityName = $scope.cityName
+      
+      if (cityName != null) {
+          weatherDataUrl += cityName
+          weatherForecastUrl += cityName
+      }
+
+      console.log(weatherDataUrl)
+      console.log(weatherForecastUrl)
+
+      $http.get(weatherDataUrl)
            .then(({data: weatherData}) => {
-            $http.get('http://localhost:8080/forecast/' + cityName)
-                 .then(({data: weatherPredictionData}) => {
-                      aModel = model(weatherData, weatherPredictionData)
-                      $scope.model.latestDataOfEachType = aModel.showLatestWeatherData()
-                      $scope.model.minimumTemperatureData = aModel.showMinimumTemperatureWeatherData()
-                      $scope.model.maximumTemperatureData = aModel.showMaximumTemperatureWeatherData()
-                      $scope.model.totalPrecipitation = aModel.showTotalPrecipitation()
-                      $scope.model.averageWindSpeed = aModel.showAverageWindSpeed()
-                      $scope.model.averageCloudCoverage = aModel.showAverageCloudCoverage()
-                      $scope.model.dominantWindDirection = aModel.showDominantWindDirection() 
-                      $scope.model.weatherPredictions = aModel.showWeatherForecastData()
-              })
-          })
-        }
-      })
+           $http.get(weatherForecastUrl)
+                .then(({data: weatherPredictionData}) => {
+                    aModel = model(weatherData, weatherPredictionData)
+                    $scope.model.latestDataOfEachType = aModel.showLatestWeatherData()
+                    $scope.model.minimumTemperatureData = aModel.showMinimumTemperatureWeatherData()
+                    $scope.model.maximumTemperatureData = aModel.showMaximumTemperatureWeatherData()
+                    $scope.model.totalPrecipitation = aModel.showTotalPrecipitation()
+                    $scope.model.averageWindSpeed = aModel.showAverageWindSpeed()
+                    $scope.model.averageCloudCoverage = aModel.showAverageCloudCoverage()
+                    $scope.model.dominantWindDirection = aModel.showDominantWindDirection() 
+                    $scope.model.weatherPredictions = aModel.showWeatherForecastData()
+            })         
+      }) 
+}
